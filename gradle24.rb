3dcc78ab440a9d71710e0e456323485d75c52cee
Francis Rangel <francis.rangel@gmail.com>
2015-07-30 16:24:07 -0300

    updates description

--- gradle24.rb
+++ gradle24.rb
@@ -1,5 +1,5 @@
 class Gradle24 < Formula
-  desc "Build automation tool"
+  desc "Gradle build automation tool"
   homepage "http://www.gradle.org/"
   url "https://services.gradle.org/distributions/gradle-2.4-bin.zip"
   sha256 "c4eaecc621a81f567ded1aede4a5ddb281cc02a03a6a87c4f5502add8fc2f16f"
